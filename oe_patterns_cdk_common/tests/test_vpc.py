dbba620cf8b67815f011f7bbbf1aee428cb724c4
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-03-21 17:09:09 -0700

    working initial vpc test

--- oe_patterns_cdk_common/tests/test_vpc.py
+++ oe_patterns_cdk_common/tests/test_vpc.py
@@ -6,10 +6,8 @@ from aws_cdk import (
 from oe_patterns_cdk_common import Vpc
 
 def test_vpc():
-  app = core.App()
-  stack = Vpc(app, "test")
+  stack = core.Stack()
+  vpc = Vpc(stack, "test")
   template = assertions.Template.from_stack(stack)
 
-  template.has_resource_properties("AWS::SQS::Queue", {
-    "VisibilityTimeout": 300
-  })
+  template.resource_count_is("AWS::EC2::VPC", 1)
