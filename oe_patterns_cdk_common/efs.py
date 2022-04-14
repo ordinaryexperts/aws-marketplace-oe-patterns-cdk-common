aba4c937d9fc30f02ab8a43cdb042dde3d724f17
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-04-13 23:13:24 -0700

    EFS updates

--- oe_patterns_cdk_common/efs.py
+++ oe_patterns_cdk_common/efs.py
@@ -19,110 +19,147 @@ class Efs(core.Construct):
             **props):
         super().__init__(scope, id, **props)
 
-        efs_automatic_backups_enabled_param = core.CfnParameter(
+        self.efs_automatic_backups_enabled_param = core.CfnParameter(
             self,
             "EfsAutomaticBackupsStatus",
             allowed_values=[ "ENABLED", "DISABLED" ],
             default="ENABLED",
             description="Optional: status of automatic backups of EFS"
         )
-        efs_transition_to_ia_param = core.CfnParameter(
+        self.efs_automatic_backups_enabled_param.override_logical_id(f"{id}AutomaticBackupsStatus")
+        self.efs_transition_to_ia_param = core.CfnParameter(
             self,
             "EfsTransitionToIa",
             allowed_values=[ "", "AFTER_7_DAYS", "AFTER_14_DAYS", "AFTER_30_DAYS", "AFTER_60_DAYS", "AFTER_90_DAYS" ],
             default="",
             description="Describes the period of time that a file is not accessed, after which it transitions to IA storage. Metadata operations such as listing the contents of a directory don't count as file access events."
         )
-        efs_transition_to_primary_storage_class_param = core.CfnParameter(
+        self.efs_transition_to_ia_param.override_logical_id(f"{id}TransitionToIa")
+        self.efs_transition_to_primary_storage_class_param = core.CfnParameter(
             self,
             "EfsTransitionToPrimaryStorageClass",
             allowed_values=[ "", "AFTER_1_ACCESS" ],
             default="",
             description="Describes when to transition a file from IA storage to primary storage. Metadata operations such as listing the contents of a directory don't count as file access events."
         )
-        efs_transition_to_ia_enabled_condition = core.CfnCondition(
+        self.efs_transition_to_primary_storage_class_param.override_logical_id(f"{id}TransitionToPrimaryStorageClass")
+        self.efs_transition_to_ia_enabled_condition = core.CfnCondition(
             self,
             "TransitionToIaEnabledCondition",
-            expression=core.Fn.condition_not(core.Fn.condition_equals(efs_transition_to_ia_param.value, ""))
+            expression=core.Fn.condition_not(core.Fn.condition_equals(self.efs_transition_to_ia_param.value, ""))
         )
-        efs_transition_to_primary_storage_class_enabled_condition = core.CfnCondition(
+        self.efs_transition_to_ia_enabled_condition.override_logical_id(f"{id}TransitionToIaEnabledCondition")
+        self.efs_transition_to_primary_storage_class_enabled_condition = core.CfnCondition(
             self,
             "TransitionToPrimaryStorageClassEnabledCondition",
-            expression=core.Fn.condition_not(core.Fn.condition_equals(efs_transition_to_primary_storage_class_param.value, ""))
+            expression=core.Fn.condition_not(core.Fn.condition_equals(self.efs_transition_to_primary_storage_class_param.value, ""))
         )
+        self.efs_transition_to_primary_storage_class_enabled_condition.override_logical_id(f"{id}TransitionToPrimaryStorageClassEnabledCondition")
         # efs
-        efs_sg = aws_ec2.CfnSecurityGroup(
+        self.efs_sg = aws_ec2.CfnSecurityGroup(
             self,
             "EfsSg",
             group_description="{}/Efs".format(core.Aws.STACK_NAME),
             vpc_id=vpc.id()
         )
-        efs_sg_ingress = aws_ec2.CfnSecurityGroupIngress(
+        self.efs_sg.override_logical_id(f"{id}Sg")
+        self.efs_sg_ingress = aws_ec2.CfnSecurityGroupIngress(
             self,
             "EfsSgIngress",
             description="Allow EFS traffic from AppSg to EfsSg",
             from_port=2049,
-            group_id=efs_sg.ref,
+            group_id=self.efs_sg.ref,
             ip_protocol="tcp",
             source_security_group_id=app_sg.ref,
             to_port=2049
         )
-        efs = aws_efs.CfnFileSystem(
+        self.efs_sg.override_logical_id(f"{id}SgIngress")
+        self.efs = aws_efs.CfnFileSystem(
             self,
             "AppEfs",
             encrypted=True,
             backup_policy=aws_efs.CfnFileSystem.BackupPolicyProperty(
-                status=efs_automatic_backups_enabled_param.value_as_string
+                status=self.efs_automatic_backups_enabled_param.value_as_string
             ),
             lifecycle_policies=[
                 core.Fn.condition_if(
-                    efs_transition_to_ia_enabled_condition.logical_id,
+                    self.efs_transition_to_ia_enabled_condition.logical_id,
                     aws_efs.CfnFileSystem.LifecyclePolicyProperty(
-                        transition_to_ia=efs_transition_to_ia_param.value_as_string
+                        transition_to_ia=self.efs_transition_to_ia_param.value_as_string
                     ),
                     core.Aws.NO_VALUE
                 ),
                 core.Fn.condition_if(
-                    efs_transition_to_primary_storage_class_enabled_condition.logical_id,
+                    self.efs_transition_to_primary_storage_class_enabled_condition.logical_id,
                     aws_efs.CfnFileSystem.LifecyclePolicyProperty(
-                        transition_to_primary_storage_class=efs_transition_to_primary_storage_class_param.value_as_string
+                        transition_to_primary_storage_class=self.efs_transition_to_primary_storage_class_param.value_as_string
                     ),
                     core.Aws.NO_VALUE
                 )
             ]
         )
+        self.efs.override_logical_id(f"App{id}")
         # taskcat lint fails on valid LifecyclePolicy configurations
-        efs.cfn_options.metadata = {
+        self.efs.cfn_options.metadata = {
             "cfn-lint": {
                 "config": {
                     "ignore_checks": ['E3002']
                 }
             }
         }
-        core.Tags.of(efs).add("Name", "{}/Efs".format(core.Aws.STACK_NAME))
-        efs_mount_target1 = aws_efs.CfnMountTarget(
+        core.Tags.of(self.efs).add("Name", "{}/Efs".format(core.Aws.STACK_NAME))
+        self.efs_mount_target1 = aws_efs.CfnMountTarget(
             self,
             "AppEfsMountTarget1",
-            file_system_id=efs.ref,
-            security_groups=[ efs_sg.ref ],
+            file_system_id=self.efs.ref,
+            security_groups=[ self.efs_sg.ref ],
             subnet_id=vpc.private_subnet1_id()
         )
-        efs_mount_target2 = aws_efs.CfnMountTarget(
+        self.efs_mount_target1.override_logical_id(f"App{id}MountTarget1")
+        self.efs_mount_target2 = aws_efs.CfnMountTarget(
             self,
             "AppEfsMountTarget2",
-            file_system_id=efs.ref,
-            security_groups=[ efs_sg.ref ],
+            file_system_id=self.efs.ref,
+            security_groups=[ self.efs_sg.ref ],
             subnet_id=vpc.private_subnet2_id()
         )
+        self.efs_mount_target2.override_logical_id(f"App{id}MountTarget2")
                     # {
                     #     "Label": {
                     #         "default": "EFS Configuration"
                     #     },
                     #     "Parameters": [
-                    #         efs_automatic_backups_enabled_param.logical_id,
+                    #         self.efs_automatic_backups_enabled_param.logical_id,
                     #         efs_transition_to_ia_param.logical_id,
                     #         efs_transition_to_primary_storage_class_param.logical_id
                     #     ]
                     # },
 
+
+    def metadata_parameter_group(self):
+        return [
+            {
+                "Label": {
+                    "default": "EFS Configuration"
+                },
+                "Parameters": [
+                    self.efs_automatic_backups_enabled_param.logical_id,
+                    self.efs_transition_to_ia_param.logical_id,
+                    self.efs_transition_to_primary_storage_class_param.logical_id
+                ],
+            }
+        ]
+
+    def metadata_parameter_labels(self):
+        return {
+            self.efs_automatic_backups_enabled_param.logical_id: {
+                "default": "EFS Automatic Backups Enabled"
+            },
+            self.efs_transition_to_ia_param.logical_id: {
+                "default": "EFS Transition to IA"
+            },
+            self.efs_transition_to_primary_storage_class_param: {
+                "default": "EFS Transition to Primary Storage Class"
+            }
+        }
 
