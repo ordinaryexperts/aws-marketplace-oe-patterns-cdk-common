ca2694fcad447bfbe681f98e754f4ba8721bc4f6
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-04-14 21:51:22 -0700

    removing wip aurora construct for now

--- oe_patterns_cdk_common/aurora_mysql_rds.py
+++
@@ -1,9 +0,0 @@
-from aws_cdk import (
-    aws_ec2,
-    core
-)
-
-class AuroraMysqlRds(core.Construct):
-
-    def __init__(self, scope: core.Construct, id: str, **props):
-        super().__init__(scope, id)
