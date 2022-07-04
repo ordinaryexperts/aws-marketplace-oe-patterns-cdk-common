ef99b2b5d9175117ac68f3b34954bcba173f06e0
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-07-03 23:09:35 -0700

    adding DNS

---
+++ oe_patterns_cdk_common/tests/test_dns.py
@@ -0,0 +1,20 @@
+from aws_cdk import (
+  assertions,
+  aws_ec2,
+  Stack
+)
+
+from oe_patterns_cdk_common.vpc import Vpc
+from oe_patterns_cdk_common.dns import Dns
+from oe_patterns_cdk_common.alb import Alb
+from oe_patterns_cdk_common.asg import Asg
+
+def test_dns():
+  stack = Stack()
+  vpc = Vpc(stack, "TestVpc")
+  asg = Asg(stack, "TestAsg", vpc=vpc)
+  alb = Alb(stack, "TestAlb", asg=asg, vpc=vpc)
+  dns = Dns(stack, "TestDns", alb=alb)
+  template = assertions.Template.from_stack(stack)
+
+  template.resource_count_is("AWS::ElasticLoadBalancingV2::LoadBalancer", 1)
