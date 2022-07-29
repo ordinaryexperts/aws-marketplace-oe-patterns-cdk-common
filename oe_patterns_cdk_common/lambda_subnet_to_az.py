a627a4a13e466313c5945da17daf828cbb3872c9
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-07-28 23:54:36 -0700

    try catch for lambda

--- oe_patterns_cdk_common/lambda_subnet_to_az.py
+++ oe_patterns_cdk_common/lambda_subnet_to_az.py
@@ -2,10 +2,13 @@ import boto3
 import json
 import cfnresponse
 def handler(event, context):
-    print(event)
-    subnet_id = event['ResourceProperties']['subnet_id']
-    client = boto3.client('ec2')
-    response = client.describe_subnets(SubnetIds=[subnet_id])
-    responseData = {'az': response['Subnets'][0]['AvailabilityZone']}
-    print(responseData)
-    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+    try:
+        print(event)
+        subnet_id = event['ResourceProperties']['subnet_id']
+        client = boto3.client('ec2')
+        response = client.describe_subnets(SubnetIds=[subnet_id])
+        responseData = {'az': response['Subnets'][0]['AvailabilityZone']}
+        print(responseData)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+    except Exception:
+        cfnresponse.send(event, context, cfnresponse.FAILED)
