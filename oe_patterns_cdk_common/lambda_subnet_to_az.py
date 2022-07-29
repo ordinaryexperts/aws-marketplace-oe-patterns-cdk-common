d68f2bf118c1fc7c5e33dddb568233758ba4683d
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-07-28 23:00:19 -0700

    lambda updates

--- oe_patterns_cdk_common/lambda_subnet_to_az.py
+++ oe_patterns_cdk_common/lambda_subnet_to_az.py
@@ -3,6 +3,9 @@ import json
 import cfnresponse
 def handler(event, context):
     print(event)
-    responseData = {}
-    responseData['az'] = 'us-east-1b'
+    subnet_id = event['ResourceProperties']['subnet_id']
+    client = boto3.client('ec2')
+    response = client.describe_subnets(SubnetIds=[subnet_id])
+    responseData = {'az': response['Subnets'][0]['AvailabilityZone']}
+    print(responseData)
     cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
