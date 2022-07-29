669017b71725af6f40c73ba69f391af17d6ab68b
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-07-28 20:00:13 -0700

    trying updated lambda

--- oe_patterns_cdk_common/lambda_subnet_to_az.py
+++ oe_patterns_cdk_common/lambda_subnet_to_az.py
@@ -1,1 +1,8 @@
-print('hi')
+import boto3
+import json
+import cfnresponse
+def handler(event, context):
+    responseValue = int(event['ResourceProperties']['Input']) * 5
+    responseData = {}
+    responseData['Data'] = responseValue
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, "CustomResourcePhysicalID")
