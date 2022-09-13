3aac74363f2ebc39f40691e2faab30218229e709
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-09-12 20:35:36 -0700

    more explicit egress rules

--- oe_patterns_cdk_common/alb.py
+++ oe_patterns_cdk_common/alb.py
@@ -44,6 +44,13 @@ class Alb(Construct):
             self,
             "AlbSg",
             group_description="{}/AlbSg".format(Aws.STACK_NAME),
+            security_group_egress=[
+                aws_ec2.CfnSecurityGroup.EgressProperty(
+                    ip_protocol="-1",
+                    cidr_ip="0.0.0.0/0",
+                    description="all IPv4 egress traffic allowed"
+                )
+            ],
             vpc_id=vpc.id()
         )
         self.sg.override_logical_id(f"{id}Sg")
