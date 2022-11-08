3f6f3edea9f2a7aa17820706cf0ba01b01c1b0bd
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-11-08 11:41:12 -0800

    adding EBS support

--- oe_patterns_cdk_common/open_search_service.py
+++ oe_patterns_cdk_common/open_search_service.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     aws_ec2,
     aws_kms,
-    aws_opensearchservice
+    aws_opensearchservice,
+    CfnParameter
 )
 
 from constructs import Construct
@@ -17,6 +18,107 @@ class OpenSearchService(Construct):
             default_instance_type: str = 'm5.large.search',
             **props):
         super().__init__(scope, id, **props)
+
+        self.default_instance_type = default_instance_type
+        self.default_allowed_instance_types = [
+            "t2.micro.search",
+            "t2.small.search",
+            "t2.medium.search",
+            "t3.small.search",
+            "t3.medium.search",
+            "m3.medium.search",
+            "m3.large.search",
+            "m3.xlarge.search",
+            "m3.2xlarge.search",
+            "m4.large.search",
+            "m4.xlarge.search",
+            "m4.2xlarge.search",
+            "m4.4xlarge.search",
+            "m4.10xlarge.search",
+            "m5.large.search",
+            "m5.xlarge.search",
+            "m5.2xlarge.search",
+            "m5.4xlarge.search",
+            "m5.12xlarge.search",
+            "m6g.large.search",
+            "m6g.xlarge.search",
+            "m6g.2xlarge.search",
+            "m6g.4xlarge.search",
+            "m6g.8xlarge.search",
+            "m6g.12xlarge.search",
+            "c4.large.search",
+            "c4.xlarge.search",
+            "c4.2xlarge.search",
+            "c4.4xlarge.search",
+            "c4.8xlarge.search",
+            "c5.large.search",
+            "c5.xlarge.search",
+            "c5.2xlarge.search",
+            "c5.4xlarge.search",
+            "c5.9xlarge.search",
+            "c5.18xlarge.search",
+            "c6g.large.search",
+            "c6g.xlarge.search",
+            "c6g.2xlarge.search",
+            "c6g.4xlarge.search",
+            "c6g.8xlarge.search",
+            "c6g.12xlarge.search",
+            "r3.large.search",
+            "r3.xlarge.search",
+            "r3.2xlarge.search",
+            "r3.4xlarge.search",
+            "r3.8xlarge.search",
+            "r4.large.search",
+            "r4.xlarge.search",
+            "r4.2xlarge.search",
+            "r4.4xlarge.search",
+            "r4.8xlarge.search",
+            "r4.16xlarge.search",
+            "r5.large.search",
+            "r5.xlarge.search",
+            "r5.2xlarge.search",
+            "r5.4xlarge.search",
+            "r5.12xlarge.search",
+            "r6g.large.search",
+            "r6g.xlarge.search",
+            "r6g.2xlarge.search",
+            "r6g.4xlarge.search",
+            "r6g.8xlarge.search",
+            "r6g.12xlarge.search",
+            "r6gd.large.search",
+            "r6gd.xlarge.search",
+            "r6gd.2xlarge.search",
+            "r6gd.4xlarge.search",
+            "r6gd.8xlarge.search",
+            "r6gd.12xlarge.search",
+            "r6gd.16xlarge.search",
+            "i2.xlarge.search",
+            "i2.2xlarge.search",
+            "i3.large.search",
+            "i3.xlarge.search",
+            "i3.2xlarge.search",
+            "i3.4xlarge.search",
+            "i3.8xlarge.search",
+            "i3.16xlarge.search"
+        ]
+
+        self.open_search_service_ebs_volume_size_param = CfnParameter(
+            self,
+            "OpenSearchServiceEbsVolumeSize",
+            default=10,
+            description="Required: The size of the EBS volume for the OpenSearch node.",
+            type="Number"
+        )
+        self.open_search_service_ebs_volume_size_param.override_logical_id(f"{id}EbsVolumeSize")
+
+        self.open_search_service_node_type_param = CfnParameter(
+            self,
+            "OpenSearchServiceNodeType",
+            allowed_values=self.default_allowed_instance_types,
+            default=self.default_instance_type,
+            description="Required: Instance type for the OpenSearch Service nodes."
+        )
+        self.open_search_service_node_type_param.override_logical_id(f"{id}NodeType")
 
         self.key = aws_kms.Key(
             self,
@@ -58,17 +160,18 @@ class OpenSearchService(Construct):
             cluster_config=aws_opensearchservice.CfnDomain.ClusterConfigProperty(
                 dedicated_master_enabled=False,
                 instance_count=1,
-                instance_type="m5.large.search",
+                instance_type=self.open_search_service_node_type_param.value_as_string,
                 zone_awareness_enabled=False
             ),
             domain_endpoint_options=aws_opensearchservice.CfnDomain.DomainEndpointOptionsProperty(
                 enforce_https=True,
                 tls_security_policy="Policy-Min-TLS-1-2-2019-07"
             ),
-            # domain_name="domainName",
-            # ebs_options=aws_opensearchservice.CfnDomain.EBSOptionsProperty(
-            #     ebs_enabled=False
-            # ),
+            ebs_options=aws_opensearchservice.CfnDomain.EBSOptionsProperty(
+                ebs_enabled=True,
+                volume_size=10,
+                volume_type="gp3"
+            ),
             encryption_at_rest_options=aws_opensearchservice.CfnDomain.EncryptionAtRestOptionsProperty(
                 enabled=True,
                 kms_key_id=self.key.key_id
