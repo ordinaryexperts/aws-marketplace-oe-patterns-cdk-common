6d8c50c601aac2242f07a73d6a5687734ce3f484
Dylan Vaughn <dylan@ordinaryexperts.com>
2025-03-20 13:06:06 -0700

    debugging

--- oe_patterns_cdk_common/script_attach_ebs.sh
+++ oe_patterns_cdk_common/script_attach_ebs.sh
@@ -51,6 +51,9 @@ fi
 log "Device detected: $DEVICE"
 
 file -s $DEVICE
+log "Sleeping for 10 seconds..."
+sleep 10
+file -s $DEVICE
 file -s $DEVICE | grep -iv xfs &> /dev/null
 if [ $? == 0 ]; then
   log "No filesystem detected, formatting as XFS"
